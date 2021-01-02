f7d0e0cb7e9c4641d4a6bc8ada73d270df2c4a17
pytran3 <34180469+pytran3@users.noreply.github.com>
2021-01-02 18:22:19 +0900

    add flow

--- lib/graph.cpp
+++ lib/graph.cpp
@@ -1,3 +1,17 @@
+#include <vector>
+
+using namespace std;
+using ll = long long;
+const int inf = 1e9;
+
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep_from(i, j, n) for (ll i = (j); i < (n); ++i)
+#define rep_rev(i, n) for (ll i = n-1; i >= 0; --i)
+#define rep_from_rev(i, j, n) for (ll i = n-1; i >= j; --i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) ll(x.size())
+
+
 struct DAG {
 private:
     vector<int> visited;
@@ -61,7 +75,7 @@ struct DoublingLCA {
       if(depth[n1] < depth[n2]) swap(n1, n2);
       n1 = get_parent(n1, depth[n1] - depth[n2]);
       if(n1 == n2) return n1;
-      REP_REV(i, 31) {
+      rep_rev(i, 31) {
         if(1<<i > depth[n1]) continue;
         if (d_parents[i][n1] != d_parents[i][n2] ) {
           n1 = d_parents[i][n1];
@@ -84,5 +98,72 @@ private:
         dfs(path);
         path.erase(--path.end());
       }
+    }
+};
+
+struct FlowGraph {
+    struct Edge {
+        int rev, from, to, cap;
+        Edge(int r, int f, int t, int c): rev(r), from(f), to(t), cap(c){};
+    };
+
+    vector<vector<Edge>> list;
+    FlowGraph(int n = 0) : list(n) {};
+
+    int size() {
+      return sz(list);
+    }
+
+    vector<Edge> &operator [](int i) {
+      return list[i];
+    }
+
+    Edge& redge(const Edge e) {
+      return list[e.to][e.rev];
+    }
+
+    void runFlow(Edge &e, int f) {
+      e.cap -= f;
+      redge(e).cap += f;
+    }
+
+    void addEdge(int from, int to, int cap) {
+      int fromRev = sz(list[from]);
+      int toRev = sz(list[to]);
+      list[from].emplace_back(toRev, from, to, cap);
+      list[to].emplace_back(fromRev, to, from, 0);
+    }
+};
+
+struct Flow {
+    vector<int> seen;
+
+    Flow() {};
+
+    int flow(FlowGraph &g, int v, int t, int f) {
+      if(v == t) return f;
+      seen[v] = 1;
+
+      for(auto &e: g[v]) {
+        if(seen[e.to]) continue;
+        if(e.cap == 0) continue;
+
+        int cap = flow(g, e.to, t, min(e.cap, f));
+        if(cap == 0) continue;
+        g.runFlow(e, cap);
+        return cap;
+      }
+      return 0;
+    }
+
+    int solve(FlowGraph &g, int s, int t, int f) {
+      int ret = 0;
+      for(;;) {
+        seen.assign(sz(g), 0);
+        int cap = flow(g, s, t, f);
+        if(cap == 0) break;
+        ret += cap;
+      }
+      return ret;
     }
 };
