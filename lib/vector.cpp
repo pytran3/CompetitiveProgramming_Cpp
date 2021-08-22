da6967106fe8129ee6165a233e82699890870315
pytran3 <34180469+pytran3@users.noreply.github.com>
2021-08-22 22:49:21 +0900

    Add CompressionVector

---
+++ lib/vector.cpp
@@ -0,0 +1,15 @@
+#define all(x) (x).begin(),(x).end()
+
+
+template<typename T>
+struct CompressionVector {
+    vector<T> v;
+    CompressionVector(const vector<T> & v) noexcept {
+      this->v = v;
+      sort(all(this->v)); this->v.erase(unique(all(this->v)), this->v.end());
+    }
+
+    int index_of(T x) {
+      return lower_bound(all(v), x) - v.begin();
+    }
+};
